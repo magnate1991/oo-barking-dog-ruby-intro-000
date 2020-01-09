bec107ace2d8c36994362d39c49befb1c2821856
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-09 13:41:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,7 @@
-# Your code goes here!
-class Dog 
- 
-  def initialize(bark)
-    @bark = bark
-  end
-  
-  def bark=(bark)
-    @bark = bark
-  end 
-    
+class Dog
+  attr_accessor :name
+
   def bark
-    @bark
+    puts "woof!"
   end
 end
-
-
-fido.name = "Fido"
-fido.name
-
-fido.bark 
-woof!
